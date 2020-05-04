e915b13944218836812b9884336c0c4264270294
KatieB-24 <60154841+KatieB-24@users.noreply.github.com>
2020-05-04 19:24:33 +0100

    this is my first commit

---
+++ Project/js/app.js
@@ -0,0 +1,125 @@
+// DATA TYPES 
+
+// DATA TYPES EXERCISE
+
+// IF/ELSE STATEMENTS 
+
+// IF/ELSE EXERCISE
+
+// ARRAY 
+
+// ARRAY EXERCISE
+
+// FINAL CLASS EXERCISE
+
+// this is a comment
+
+// console.log("hello world")
+
+// console.info
+
+// console.error("hello error ")
+
+// alert("hello there")
+
+// prompt("what is your name")
+
+// var name = "katie"
+// console.log(name)
+
+// var surname = "bruce"
+// console.log(name + " " + surname)
+
+// console.log(`Hello, my name is ${name} ${surname} and I'm learning JavaScript`)
+
+// var name = prompt("What is your name?")
+
+// alert(`hi ${name}`)
+
+// var int1 = 30
+
+// console.log(typeof int1 + 10)
+
+// var float1 = 1.5
+
+// var float2 = 1.6
+
+// console.log(float1 +float2)
+
+// var tvShow = "Friends"
+
+// var characters = "6"
+
+// console.log(characters)
+
+// characters = 8
+// console.log(characters)
+
+// var bool = true
+// console.log(bool)
+
+// bool = false
+// console.log(bool)
+
+// var nothing = null
+// console.log(null)
+
+// var hasShowFinished = true
+// console.log(hasShowFinished)
+
+// console.log(Boolean(0))
+
+// console.log(Boolean(1))
+
+// var value = 2
+
+// if (value === 0) {
+//     console.log("I will not run")
+// } else {
+//     console.log("I will run")
+// }
+
+// var fruit = ["apple", "banana", "pear", "pineapple"]
+
+// console.log(fruit)
+// console.log(fruit [0]); "apple"
+// console.log(fruit [3]); "pear"
+
+// console.log(fruit.length)
+
+// fruit.push("apricot")
+// console.log(fruit)
+
+// fruit.shift()
+// console.log(fruit)
+
+// fruit.splice(1, 0, "peach", "nectarine")
+// console.log(fruit)
+
+// var countries = ["France", "Germany"]
+// console.log(countries)
+
+// countries.push("Italy")
+// console.log(countries)
+
+// countries.shift()
+// console.log(countries)
+
+// countries.splice(1, 0, "China")
+// console.log(countries)
+
+// var countryList = countries.join(", ")
+// console.log(countryList)
+
+// 1. Create an alert with instructions
+// 2. Create a variable to store user input
+
+var rsvp = prompt("Are you sure you want to leave this page? Type yes/no.")
+
+if (rsvp === "y") {
+    alert("Any unsaved changes will be lost...")
+} else if (rsvp === "n") {
+    alert("Great, make sure to save your work!")
+} else {
+    alert("It's an important decision! Please type yes or no")
+}
